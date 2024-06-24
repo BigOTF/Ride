565cc78b1dc0ad7075a2858db3283a803c322df0
Olorunfemi Olamide <166822505+BigOTF@users.noreply.github.com>
2024-06-24 18:49:03 +0100

    Uninstall the old tailwind config here and reinstall a new one and also finished working on the home pages. Moving to the footer layout

--- app/page.js
+++ app/page.js
@@ -9,6 +9,7 @@ import mail from '@/public/mail.svg'
 
 import { exploreData } from '@/data/Explore'
 import { TestimonialData } from '@/data/Testimonial'
+import { Faqs } from '@/data/FAQ'
 
 import { IoMdClock } from "react-icons/io";
 import { FaHandHoldingUsd } from "react-icons/fa";
@@ -21,7 +22,8 @@ import { FaToolbox } from "react-icons/fa6";
 import { IoIosSnow } from "react-icons/io";
 import { TbAutomaticGearbox } from "react-icons/tb";
 import { FaStar } from "react-icons/fa6";
-
+import { FaAngleDown } from "react-icons/fa";
+import { FaAngleUp } from 'react-icons/fa'
 
 const Home = () => {
   const router = useRouter();
@@ -98,6 +100,24 @@ const Home = () => {
             <div className='bg-gray-900 h-10'></div>
 
 
+          </div>
+        )
+      })
+    )
+  }
+
+  const FaqDataSection = () => {
+    return (
+      Faqs.map((data) => {
+        return (
+          <div key={data.id} className='flex items-center justify-between border-b border-black px-3 py-3'>
+            <div className='flex flex-col gap-2'>
+              <p>{data.question}</p>
+            </div>
+            <div>
+              <FaAngleDown className='cursor-pointer'/>
+            </div>
+           
           </div>
         )
       })
@@ -386,6 +406,16 @@ const Home = () => {
           <TestimonialDataSection />
         </div>
       </section>
+
+      <section className='px-20 flex flex-col gap-10'>
+        <p className='text-2xl font-semibold text-black text-center'>Frequently Asked Questions</p>
+
+        <div>
+          <FaqDataSection />
+        </div>
+      </section>
+
+      
     
     </main>
   )
---
+++ data/FAQ.js
@@ -0,0 +1,38 @@
+export const Faqs = [
+    {
+        id: 1,
+        question: 'Can I get my Car delivered to me?',
+        answer: 'Yes',
+        checked: false
+    },
+    {
+        id: 2,
+        question: 'What Is the cancellation policy on CRida?',
+        answer: 'Yes',
+        checked: false
+    },
+    {
+        id: 3,
+        question: 'Can I book a rental car for someone else?',
+        answer: 'Yes',
+        checked: false
+    },
+    {
+        id: 4,
+        question: 'Do you offer long term lease?',
+        answer: 'Yes',
+        checked: false
+    },
+    {
+        id: 5,
+        question: 'Can the vehicle be rented for travel outside the outside?',
+        answer: 'Yes',
+        checked: false
+    },
+    {
+        id: 6,
+        question: 'Can I get my Car delivered to me?',
+        answer: 'Yes',
+        checked: false
+    },
+]
